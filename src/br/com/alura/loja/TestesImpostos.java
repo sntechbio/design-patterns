fcef77751bfd54fe069bd352c53f5013ead4d1bd
sntechbio <lnascimentodesouza@gmail.com>
2023-08-24 21:21:28 -0300

    padrão strategy

---
+++ src/br/com/alura/loja/TestesImpostos.java
@@ -0,0 +1,16 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class TestesImpostos {
+
+    public static void main(String[] args) {
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+        CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
+        System.out.println(calculadoraDeImpostos.calcular(orcamento, new ICMS()));
+    }
+}
